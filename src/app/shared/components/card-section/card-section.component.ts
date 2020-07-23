0a0e705b51f2416f452183af072f7a203a2856b9
Епишев Владимир <epishevvv@it2g.ru>
2020-07-23 23:10:35 +0300

    Created separate card component for reuse

---
+++ src/app/shared/components/card-section/card-section.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-section',
+  templateUrl: './card-section.component.html',
+  styleUrls: ['./card-section.component.scss']
+})
+export class CardSectionComponent {
+
+  @Input() heading = '';
+}
