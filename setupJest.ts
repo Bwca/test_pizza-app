ca1db9ec79deac4c4a500804cad04d7fed7b7b94
Volodymyr <i.m.bwca@gmail.com>
2020-10-08 20:42:36 +0300

    Got everything running

--- setupJest.ts
+++ setupJest.ts
@@ -1,2 +1,5 @@
+import { toMatchImageSnapshot } from 'jest-image-snapshot';
 import 'jest-preset-angular';
 
+// tslint:disable-next-line:no-any
+expect.extend({ toMatchImageSnapshot });
--- src/app/shared/components/no-items/no-items.component.spec.ts
+++ src/app/shared/components/no-items/no-items.component.spec.ts
@@ -1,6 +1,8 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { generateImage } from 'jsdom-screenshot';
+
 import { NoItemsComponent } from './no-items.component';
 
 describe('NoItemsComponent', () => {
@@ -22,5 +24,10 @@ describe('NoItemsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('has no visual regressions', async () => {
+    const screenshot = await generateImage();
+    expect(screenshot).toMatchImageSnapshot();
   });
 });
